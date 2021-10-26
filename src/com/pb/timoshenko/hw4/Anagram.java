455c28c18b643af938926596b72f88c44f969460
Timoshenko Andrey <andretimua@gmail.com>
2021-10-26 09:39:31 +0300

    CapitalLetter and Anagram is WORK!

--- src/com/pb/timoshenko/hw4/Anagram.java
+++ src/com/pb/timoshenko/hw4/Anagram.java
@@ -1,25 +1,44 @@
 package com.pb.timoshenko.hw4;
 
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Anagram {
 
+    //удаление всех знаков припинания
+    public static String rmPunct(String str) {
+        String res = "";
+        for (Character ch : str.toCharArray()) {
+            if(Character.isLetterOrDigit(ch))
+                res = res + ch;
+        }
+        return res;
+    }
+
+    //сортировка по алфавиту массива букв
+    static char[] sorting (String str){
+        char[] chArray = str.toCharArray();
+        Arrays.sort(chArray);
+        return chArray;
+    }
 
     public static void main(String[] args) {
-        String str = "This is String , split by StringTokenizer, created by mkyong";
-        StringTokenizer st = new StringTokenizer(str);
+//        String test1 = ("Я в мире — сирота.").toLowerCase();
+//        String test2 = ("Я в Риме — Ариост.").toLowerCase();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two phrases!");
+        System.out.print("First phrases: ");
+        String test1 = (sc.nextLine()).toLowerCase();
+        System.out.print("Second phrases: ");
+        String test2 = (sc.nextLine()).toLowerCase();
+        char[] nTest1 = sorting(rmPunct(test1));
+        char[] nTest2 = sorting(rmPunct(test2));
 
-        System.out.println("---- Split by space ------");
-        while (st.hasMoreElements()) {
-            System.out.print(st.nextElement());
+        if (Arrays.equals(nTest1, nTest2)){
+            System.out.println("OK! It's anagram");
         }
-
-        System.out.println("---- Split by comma ',' ------");
-        StringTokenizer st2 = new StringTokenizer(str, ",");
-
-        while (st2.hasMoreElements()) {
-            System.out.println(st2.nextElement());
+        else {
+            System.out.println("It isn't anagram");
         }
     }
 }
